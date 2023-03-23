e811b672aaa071c8fd1b0945233924553286ddd0
Derek <developer.duafa@gmail.com>
2023-03-23 00:52:56 +0000

    add user repository

--- repositories/user.repository.go
+++ repositories/user.repository.go
@@ -1,9 +1,10 @@
 package repositories
 
 import (
-	"github.com/dduafa/go-server/models"
 	"strings"
 
+	"github.com/dduafa/go-server/models"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
@@ -18,10 +19,7 @@ func newUserRepository(db *gorm.DB) *userRepository {
 }
 
 func (rp *userRepository) Create(user *models.User) error {
-	if err := rp.db.Create(user).Error; err != nil {
-		return err
-	}
-	return nil
+	return rp.db.Create(user).Error
 }
 
 func (rp *userRepository) FindUserByEmail(email string) (*models.User, error) {
@@ -30,4 +28,30 @@ func (rp *userRepository) FindUserByEmail(email string) (*models.User, error) {
 		return &user, err
 	}
 	return &user, nil
+}
+
+func (u *userRepository) FindAll() ([]models.User, error) {
+	var users []models.User
+	err := u.db.Find(&users).Error
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (u *userRepository) FindByID(id uuid.UUID) (models.User, error) {
+	var user models.User
+	err := u.db.First(&user, id).Error
+	if err != nil {
+		return models.User{}, err
+	}
+	return user, nil
+}
+
+func (u *userRepository) Update(user *models.User) error {
+	return u.db.Save(user).Error
+}
+
+func (u *userRepository) Delete(id uuid.UUID) error {
+	return u.db.Delete(&models.User{}, id).Error
 }
